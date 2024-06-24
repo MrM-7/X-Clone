3c21f2837ebe791a3edcd25695db9c5bbf1282fa
Mehul Raj <rmmehul7@gmail.com>
2024-06-24 11:23:25 +0530

    feat: Add authentication routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server is running");
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+// routes import
+import authRoutes from "./routes/auth.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`⚙️  Server is running at port : ${PORT}`);
+})
